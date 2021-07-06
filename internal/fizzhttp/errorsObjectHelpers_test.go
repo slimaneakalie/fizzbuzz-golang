8ee6a62a92f3e7e4b68ed96a117a384b19fcc859
slimaneakalie <slimaneakalia@gmail.com>
2021-07-06 23:24:22 +0100

    feat: add test for NewHttpErrorResponseMetadata

--- internal/fizzhttp/errorsObjectHelpers_test.go
+++ internal/fizzhttp/errorsObjectHelpers_test.go
@@ -1,1 +1,25 @@
 package fizzhttp
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("fizzhttp package - errorObjectHelpers.go", func() {
+	Context("NewHttpErrorResponseMetadata function", func() {
+		It("should map params to a valid httpErrorResponseMetadata object", func() {
+			sampleFieldsError := []*responseFieldError{
+				NewFieldError(ParamTypeMismatchErrorCode, "fieldName", "error detail"),
+			}
+			sampleErrorType := BadRequestResponseTypeCode
+
+			expected := &httpErrorResponseMetadata{
+				Type:        sampleErrorType,
+				FieldErrors: sampleFieldsError,
+			}
+			actual := NewHttpErrorResponseMetadata(sampleErrorType, sampleFieldsError)
+
+			Expect(actual).To(Equal(expected))
+		})
+	})
+})
